b732ddb1a07011ca40337587a785d39a08b6c007
tannerwride <tanner@circleci.com>
2021-10-14 11:41:15 -0600

    add first GET route

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
     ]
-    app.get('/users', function(req,res){
-         res.json({
-              success: true,
-              message: 'successfully got users. Nice!',
-              users: mockUserData
-         })
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. nice.',
+        users: mockUserData
     })
-    
-
+})
 app.listen(8000,function(){
     console.log("server is listening")
 })
